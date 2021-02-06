68d47b96799b50f5aefe10ceb2e7090b0d86ac8e
hariprasadsr <78660816+hariprasadsr@users.noreply.github.com>
2021-02-07 00:58:38 +0530

    If statement assignment

---
+++ If.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  6 23:41:41 2021
+
+@author: T480s
+"""
+
+
+
+Number1 = "0"
+Number2 = 5
+Number3 = 9
+Number1 = int(Number1)
+Number2 = int(Number2)
+Number3 = int(Number3)
+Equal = False
+
+if Number1 == Number2 or Number3 == Number1 or Number2 == Number3:
+    Equal = True
+    
+print(Equal)
